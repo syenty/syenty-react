2f681bd21afa8281bb52a524fe05e4eb6d1bcac2
syenty <tog1993@naver.com>
2021-05-02 23:35:57 +0900

    delete test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./routes/About"
 import Home  from "./routes/Home"
 import Detail  from "./routes/Detail"
 import Search  from "./routes/Search"
-import Test  from "./routes/Test"
+// import Test  from "./routes/Test"
 import Lotto  from "./routes/Lotto"
 import Navigation from "./components/Navigation"
 import "./App.css"
@@ -18,7 +18,7 @@ function App(){
       <Route path="/about" component={About}/>
       <Route path="/movie/:id" component={Detail}/>
       <Route path="/search" component={Search}/>
-      <Route path="/test" component={Test}/>
+      {/* <Route path="/test" component={Test}/> */}
       <Route path="/lotto" component={Lotto}/>
     </HashRouter>
   )
